4f03c26f0d108ed40fab0b31601f1d0989b51373
Julio Leonardo <juleolica@gmail.com>
2020-10-30 00:54:52 -0300

    Removing routing approach

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
+import Main from "./components/Main";
+
 function App() {
-  return (
-    <div className="App">
-      <p>Olá, Mundo!</p>
-    </div>
-  );
+  return <Main />;
 }
 
 export default App;
