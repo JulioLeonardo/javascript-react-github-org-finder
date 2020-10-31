d58c88ed35db50d1042200eaf8cc86c3cb8ff781
Julio Leonardo <juleolica@gmail.com>
2020-10-31 13:33:37 -0300

    Styled Org info

--- src/components/Organization/index.js
+++ src/components/Organization/index.js
@@ -1,5 +1,7 @@
 import OrgRepos from "../OrgRepos";
 
+import "./styles.css";
+
 export default function Organization({ organizationData, setFetched }) {
   const login = organizationData.login;
   const title = login.charAt(0).toUpperCase() + login.slice(1);
@@ -15,33 +17,114 @@ export default function Organization({ organizationData, setFetched }) {
 
   return (
     <div className="organization">
-      <div className="header">
+      <div className="organization-header">
         <div
           className="back"
           onClick={() => {
             setFetched(false);
           }}
         >
-          {"<"}
+          {"< Voltar"}
         </div>
         <div className="organization-header-title">
           <p>{login}</p>
         </div>
         <div className="notifications">
-          <p>Notificações</p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M12 1C8.318 1 5 3.565 5 7v4.539a3.25 3.25 0 01-.546 1.803l-2.2 3.299A1.518 1.518 0 003.519 19H8.5a3.5 3.5 0 107 0h4.982a1.518 1.518 0 001.263-2.36l-2.2-3.298A3.25 3.25 0 0119 11.539V7c0-3.435-3.319-6-7-6zM6.5 7c0-2.364 2.383-4.5 5.5-4.5s5.5 2.136 5.5 4.5v4.539c0 .938.278 1.854.798 2.635l2.199 3.299a.017.017 0 01.003.01l-.001.006-.004.006-.006.004-.007.001H3.518l-.007-.001-.006-.004-.004-.006-.001-.007.003-.01 2.2-3.298a4.75 4.75 0 00.797-2.635V7zM14 19h-4a2 2 0 104 0z"
+            ></path>
+          </svg>
         </div>
       </div>
-      <div className="avatar-div">
-        <img className="avatar" src={avatar} alt="avatar" />
-      </div>
-      <div className="info-div">
-        <p className="title">{title}</p>
-        <p className="description">{description}</p>
-        {location && <p className="info">{location}</p>}
-        {blog && <p className="info">{blog}</p>}
-        {twitter && <p className="info">{twitter}</p>}
-        {email && <p className="info">{email}</p>}
-        {verified && <p className="verified">Verified</p>}
+      <div className="avatar-info-wrap">
+        <div className="avatar-div">
+          <img className="avatar" src={avatar} alt="avatar" />
+        </div>
+        <div className="info-div">
+          <p className="title">{title}</p>
+          <p className="description">{description}</p>
+          {location && (
+            <p className="info">
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  width="16"
+                  height="16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M11.536 3.464a5 5 0 010 7.072L8 14.07l-3.536-3.535a5 5 0 117.072-7.072v.001zm1.06 8.132a6.5 6.5 0 10-9.192 0l3.535 3.536a1.5 1.5 0 002.122 0l3.535-3.536zM8 9a2 2 0 100-4 2 2 0 000 4z"
+                  ></path>
+                </svg>
+              </span>
+              {location}
+            </p>
+          )}
+          {blog && (
+            <p className="info">
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  width="16"
+                  height="16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"
+                  ></path>
+                </svg>
+              </span>
+              {blog}
+            </p>
+          )}
+          {twitter && (
+            <p className="info">
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 273.5 222.3"
+                  height="16"
+                  width="16"
+                >
+                  <path
+                    d="M273.5 26.3a109.77 109.77 0 0 1-32.2 8.8 56.07 56.07 0 0 0 24.7-31 113.39 113.39 0 0 1-35.7 13.6 56.1 56.1 0 0 0-97 38.4 54 54 0 0 0 1.5 12.8A159.68 159.68 0 0 1 19.1 10.3a56.12 56.12 0 0 0 17.4 74.9 56.06 56.06 0 0 1-25.4-7v.7a56.11 56.11 0 0 0 45 55 55.65 55.65 0 0 1-14.8 2 62.39 62.39 0 0 1-10.6-1 56.24 56.24 0 0 0 52.4 39 112.87 112.87 0 0 1-69.7 24 119 119 0 0 1-13.4-.8 158.83 158.83 0 0 0 86 25.2c103.2 0 159.6-85.5 159.6-159.6 0-2.4-.1-4.9-.2-7.3a114.25 114.25 0 0 0 28.1-29.1"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </span>
+              {"@" + twitter}
+            </p>
+          )}
+          {email && (
+            <p className="info">
+              <span>
+                <svg
+                  viewBox="0 0 16 16"
+                  version="1.1"
+                  width="16"
+                  height="16"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M1.75 2A1.75 1.75 0 000 3.75v.736a.75.75 0 000 .027v7.737C0 13.216.784 14 1.75 14h12.5A1.75 1.75 0 0016 12.25v-8.5A1.75 1.75 0 0014.25 2H1.75zM14.5 4.07v-.32a.25.25 0 00-.25-.25H1.75a.25.25 0 00-.25.25v.32L8 7.88l6.5-3.81zm-13 1.74v6.441c0 .138.112.25.25.25h12.5a.25.25 0 00.25-.25V5.809L8.38 9.397a.75.75 0 01-.76 0L1.5 5.809z"
+                  ></path>
+                </svg>
+              </span>
+              {email}
+            </p>
+          )}
+          {verified && <p className="verified">Verified</p>}
+        </div>
       </div>
       <OrgRepos login={login} totalRepos={totalRepos} />
     </div>
