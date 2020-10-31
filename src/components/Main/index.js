0a819200e7b6c42f3e4420cfc18a0049f00736eb
Julio Leonardo <juleolica@gmail.com>
2020-10-31 19:13:28 -0300

    Main component style tweaks

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import api from "../../services/api";
 import Organization from "../Organization";
 import "./styles.css";
+import octocat from "../../assets/img/octocat.png";
 
 export default function SearchOrganization() {
   const [organization, setOrganization] = useState("");
@@ -40,42 +41,47 @@ export default function SearchOrganization() {
     <>
       {!fetched && !error && !loading && (
         <div className="search-organization">
-          <div className="main-header">
-            <p className="logo">{"<Github Orgs/>"}</p>
-            <p className="app-description">
-              Encontre os perfis das organizações que usam o github e liste seus
-              repositórios
-            </p>
-          </div>
-          <div className="search-block">
-            <label htmlFor="org-search">
-              <p>Pesquise a</p> <p>organização</p>{" "}
-              <p className="last-line">
-                aqui <span id="cursor"></span>
+          <div className="main-header-search-block-wrapper">
+            <div className="main-header">
+              <p className="logo">{"<Github Orgs/>"}</p>
+              <p className="app-description">
+                Encontre os perfis das organizações que usam o github e liste
+                seus repositórios
               </p>
-            </label>
-            <div className="input-block">
-              <input
-                type="text"
-                name="org-search"
-                id="org-search"
-                onChange={handleInputChange}
-              />
-              <button type="submit" onClick={searchOrg}>
-                <svg
-                  id="search-icon"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  width="24"
-                  height="24"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M14.53 15.59a8.25 8.25 0 111.06-1.06l5.69 5.69a.75.75 0 11-1.06 1.06l-5.69-5.69zM2.5 9.25a6.75 6.75 0 1111.74 4.547.746.746 0 00-.443.442A6.75 6.75 0 012.5 9.25z"
-                  ></path>
-                </svg>
-              </button>
             </div>
+            <div className="search-block">
+              <label htmlFor="org-search">
+                <p>Pesquise a</p> <p>organização</p>{" "}
+                <p className="last-line">
+                  aqui <span id="cursor"></span>
+                </p>
+              </label>
+              <div className="input-block">
+                <input
+                  type="text"
+                  name="org-search"
+                  id="org-search"
+                  onChange={handleInputChange}
+                />
+                <button type="submit" onClick={searchOrg}>
+                  <svg
+                    id="search-icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M14.53 15.59a8.25 8.25 0 111.06-1.06l5.69 5.69a.75.75 0 11-1.06 1.06l-5.69-5.69zM2.5 9.25a6.75 6.75 0 1111.74 4.547.746.746 0 00-.443.442A6.75 6.75 0 012.5 9.25z"
+                    ></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="octocat">
+            <img id="octocat" src={octocat} alt="octocat" />
           </div>
         </div>
       )}
